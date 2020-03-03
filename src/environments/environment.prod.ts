7a0a6129881f4e492db0dd3b722d9e264a5124ac
aiallanigue <allanallanigue@gmail.com>
2020-03-03 21:39:22 +0800

    FE version 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  firebase: {}
-};
+  firebase: {
+  apiKey: 'AIzaSyDBKpRtn16XbZpQ-LJApUBYVY-Q9TExfII',
+  authDomain: 'video-on-demand-d6979.firebaseapp.com',
+  databaseURL: 'https://video-on-demand-d6979.firebaseio.com/',
+  projectId: 'video-on-demand-d6979',
+  storageBucket: 'xxxx-d58ec.appspot.com',
+  messagingSenderId: '652232134739'
+  }
+  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
-  firebase: {}
-};
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  production: true,
+  firebase: {
+  apiKey: 'AIzaSyDBKpRtn16XbZpQ-LJApUBYVY-Q9TExfII',
+  authDomain: 'video-on-demand-d6979.firebaseapp.com',
+  databaseURL: 'https://video-on-demand-d6979.firebaseio.com/',
+  projectId: 'video-on-demand-d6979',
+  storageBucket: 'xxxx-d58ec.appspot.com',
+  messagingSenderId: '652232134739'
+  }
+  };
